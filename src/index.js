ee6121808adfe68a53dc82c24e86d18695194160
GermanDelRioGuzman <110877408+GermanDelRioGuzman@users.noreply.github.com>
2024-06-16 03:15:30 +0000

    port

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
